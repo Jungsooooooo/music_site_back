06abfda65964b3eab08edf02bd170df7c71cbcd3
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-08-22 23:07:59 +0900

    security config 설정

--- src/main/java/com/music/restful/token/WebSecurityConfig.java
+++ src/main/java/com/music/restful/token/WebSecurityConfig.java
@@ -1,47 +1,45 @@
 package com.music.restful.token;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.RequiredArgsConstructor;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	  @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	        http
-	                .httpBasic().disable()
-	                .cors()     //Cors 옵션 활성화
-	                //...다른 설정들
-	                ;
-	        http.csrf().disable();
-
-
-	    }
-	 
-
+	private final JwtTokenProvider jwtTokenProvider;
 	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic().disable().cors();
+		http.authorizeRequests().antMatchers("/api/users/**").permitAll().anyRequest().authenticated().and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests();
+		http.csrf().disable();
+
+	}
+
 	@Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
+		configuration.addAllowedOriginPattern("*");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
 
-        
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
